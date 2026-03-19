5bd321e85b8c346971dcc50f82f1f9bc09638a91
zhaoyboo <zhaoyboo@outlook.com>
2026-03-19 16:06:00 -0400

    skip identity op for partial hook

--- src/transformers/models/qwen3_p/modeling_qwen3.py
+++ src/transformers/models/qwen3_p/modeling_qwen3.py
@@ -290,7 +290,7 @@ class Qwen3DecoderLayer(GradientCheckpointingLayer):
         position_embeddings: Optional[tuple[torch.Tensor, torch.Tensor]] = None,  # necessary, but kept here for BC
         **kwargs: Unpack[TransformersKwargs],
     ) -> torch.Tensor:
-        self.hook_resid_pre(hidden_states)
+        hidden_states = self.hook_resid_pre(hidden_states)
         residual = hidden_states
         hidden_states = self.input_layernorm(hidden_states)
         hidden_states = self.hook_ln1(hidden_states)
